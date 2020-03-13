35affd730f553fcd0acda5f02397bbd933173c41
Eyasu Gebrehiwot <jgebrehiwot@gmail.com>
2020-03-12 22:00:44 -0400

    This is a Commit with Ayuye

--- Ayuye.js
+++ Ayuye.js
@@ -1,3 +1,3 @@
 console.log("This is a test");
-#Ayuye is my favorite creature in the whole universe next to God 
-#Let's see that life is beautiful and good by God's grace.
+//Ayuye is my favorite creature in the whole universe next to God
+//Let's see that life is beautiful and good by God's grace.
